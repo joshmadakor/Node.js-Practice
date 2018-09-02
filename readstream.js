99f608a86ce51ea85b79c58320fd5344be130210
joshmadakor1 <39254979+joshmadakor1@users.noreply.github.com>
2018-09-01 22:56:35 -0700

    updates

---
+++ readstream.js
@@ -0,0 +1,24 @@
+/*
+var port   = 8080
+var ip     = '127.0.0.1'
+var http   = require('http');
+var server = http.createServer(function(req, res){
+  res.writeHead(200,{'Content-Type':'text/html'});
+  console.log(req.url);
+  res.end(`Welcome to ${ip}:${port}`)
+})
+
+server.listen(port, ip);
+console.log(`Server ${ip} is listening on port ${port}.`)
+*/
+
+var http = require('http');
+var fs   = require('fs');
+
+var myReadStream =  fs.createReadStream (`${__dirname}/sample.txt`, 'ascii');
+var myWriteStream = fs.createWriteStream(`${__dirname}/writeme.txt`,'ascii');
+var count = 0;
+myReadStream.on('data',function(chunk){
+  myWriteStream.write(chunk);
+  count++;
+});
